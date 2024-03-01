5a9e5cfeca46c22c1400fe09446e2a2ab64ad873
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-03-01 20:10:38 +0600

    demo dynamic card added

--- src/js/activity.js
+++ src/js/activity.js
@@ -7,6 +7,7 @@
 // Example: - https://openapi.programming-hero.com/api/videos/category/1000
 
 const buttonContainer = document.getElementById('buttonContainer');
+const cardContainer = document.getElementById('cardContainer');
 
 (() => {
     fetch('https://openapi.programming-hero.com/api/videos/categories')
@@ -14,9 +15,34 @@ const buttonContainer = document.getElementById('buttonContainer');
         .then(({data}) => {
             data.forEach((fetchedData) => {
                 const newBtn = document.createElement('button');
-                newBtn.classList.add('btn', 'mx-2');
+                newBtn.classList.add('btn', 'mx-2', 'btn-ghost', 'text-lg');
                 newBtn.innerText = `${fetchedData.category}`;
+                newBtn.addEventListener('click', () => fetchedDataByCategories(fetchedData.category_id));
                 buttonContainer.appendChild(newBtn);
             })
         })
 })();
+
+const fetchedDataByCategories = (idOfData) => {
+    fetch(`https://openapi.programming-hero.com/api/videos/category/${idOfData}`)
+        .then((res) => res.json())
+        .then(({data}) => {
+            data.forEach((fetchedData) => {
+                const newCard = document.createElement('div');
+                newCard.innerHTML = `
+                <div class="card card-compact bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                        alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                `
+                cardContainer.appendChild(newCard);
+            })
+        })
+}
